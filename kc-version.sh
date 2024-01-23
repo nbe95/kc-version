a66e44f5b194c0b1ae6e5a1ae63e212136c18212
Niklas Bettgen <niklas@bettgen.de>
2024-01-23 08:00:13 +0100

    Improve matching

--- kc-version.sh
+++ kc-version.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
-remove_leading_zeros() {
-  echo "$1" | sed 's/^0\+\(.\)/\1/'
-}
+# Abort on error
+set -e
 
-# Pad input variable with leading zeros and replace anything but numbers
-input="$(remove_leading_zeros "$(echo "$1" | sed 's/[^0-9]//g')")"
-printf -v version "%08d" "$input"
+# Reusable functions
+trim_leading_zeros() { echo "$1" | sed 's/^0\+\(.\)/\1/'; }
 
-# Match individual version components
-regex="^([0-9]{2})([0-9]{2})([0-9]{4})$"
-if [[ ! $version =~ $regex ]]; then
-  echo "Version $version has invalid format."
-  exit 1
+# Get version from CLI argument, otherwise ask git
+version="$1"
+if [[ -z "$version" ]]; then
+    version="$(git -C "$(pwd)" describe --tags --abbrev=0 2> /dev/null || true)"
+fi
+if [[ -z "$version" ]]; then
+    echo "No version tag available."
+    exit 1
 fi
 
-# Parse version components
-customer="$(remove_leading_zeros "${BASH_REMATCH[1]}")"
-year="$(remove_leading_zeros "${BASH_REMATCH[2]}")"
-majmin="$(remove_leading_zeros "${BASH_REMATCH[3]}")"
+# Try to match dot form (XX.YY.ZZZZ)
+if [[ "$version" =~ ^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4})$ ]]; then
+    customer="$(trim_leading_zeros "${BASH_REMATCH[1]}")"
+    year="$(trim_leading_zeros "${BASH_REMATCH[2]}")"
+    majmin="$(trim_leading_zeros "${BASH_REMATCH[3]}")"
+
+else
+    # Match an integer else (pad with zeros and replace anything but numbers)
+    if [[ ! "$version" =~ ^[0-9]{1,8}$ ]]; then
+        echo "Version tag '$version' has invalid format."
+        exit 2
+    fi
+    version="$(echo "$version" | sed 's/[^0-9]//g')"
+    printf -v version "%08d" "$(trim_leading_zeros "$version")"
+
+    customer="$(trim_leading_zeros "${version:0:2}")"
+    year="$(trim_leading_zeros "${version:2:2}")"
+    majmin="$(trim_leading_zeros "${version:4:4}")"
+fi
+
+# Parse remaining components
 major=$(($majmin / 100))
 minor=$(($majmin % 100))
 
