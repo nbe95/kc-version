33db0e725dc3fc5d5b882dcfa0625d1d71fb94f7
Niklas Bettgen <niklas@bettgen.de>
2024-01-28 14:14:44 +0100

    Migrate repo

--- tests/unit-test.sh
+++ tests/unit-test.sh
@@ -1,51 +1,43 @@
 #!/bin/bash
 
 DIR="$(dirname "$0")"
-KC_VERSION="$DIR/../kc-version.sh"
+VERSION="$DIR/../version.sh"
 
 # shellcheck disable=SC1091
 source "$DIR/assert/assert.sh"
 
 
 # Invalid argument format (check exit codes only)
-assert_eq "$($KC_VERSION                &> /dev/null)$?" "1" "invalid"
-assert_eq "$($KC_VERSION foo            &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION foo123         &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION foo.bar.123    &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 1.2            &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 12...33        &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 12.34.56.78    &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 12.345.678     &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 1234.56.78     &> /dev/null)$?" "2" "invalid"
-assert_eq "$($KC_VERSION 123456789      &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION               &> /dev/null)$?" "1" "invalid"
+assert_eq "$($VERSION foo           &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION foo123        &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION foo.bar.123   &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 1.2           &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 12...33       &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 12.34.56.78   &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 12.345.678    &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 1234.56.78    &> /dev/null)$?" "2" "invalid"
+assert_eq "$($VERSION 123456789     &> /dev/null)$?" "2" "invalid"
 
 
 # Basic dot notation
-assert_eq "$($KC_VERSION 0              )" "00.00.0000" "invalid"
-assert_eq "$($KC_VERSION 1              )" "00.00.0001" "invalid"
-assert_eq "$($KC_VERSION 00.00.1234     )" "00.00.1234" "invalid"
-assert_eq "$($KC_VERSION 0.6.9          )" "00.06.0009" "invalid"
-assert_eq "$($KC_VERSION 00.42.00       )" "00.42.0000" "invalid"
-assert_eq "$($KC_VERSION 99.00.0        )" "99.00.0000" "invalid"
-assert_eq "$($KC_VERSION 12.34.5678     )" "12.34.5678" "invalid"
-assert_eq "$($KC_VERSION 99.99.9999     )" "99.99.9999" "invalid"
+assert_eq "$($VERSION 0             )" "00.00.0000" "invalid"
+assert_eq "$($VERSION 1             )" "00.00.0001" "invalid"
+assert_eq "$($VERSION 00.00.1234    )" "00.00.1234" "invalid"
+assert_eq "$($VERSION 0.6.9         )" "00.06.0009" "invalid"
+assert_eq "$($VERSION 00.42.00      )" "00.42.0000" "invalid"-
+assert_eq "$($VERSION 420000        )" "00.42.0000" "invalid"
+assert_eq "$($VERSION 69000000      )" "69.00.0000" "invalid"
+assert_eq "$($VERSION 12345678      )" "12.34.5678" "invalid"
+assert_eq "$($VERSION 99999999      )" "99.99.9999" "invalid"
 
-
-# Basic integer notation
-assert_eq "$($KC_VERSION 0              )" "00.00.0000" "invalid"
-assert_eq "$($KC_VERSION 1              )" "00.00.0001" "invalid"
-assert_eq "$($KC_VERSION 420000         )" "00.42.0000" "invalid"
-assert_eq "$($KC_VERSION 69000000       )" "69.00.0000" "invalid"
-assert_eq "$($KC_VERSION 12345678       )" "12.34.5678" "invalid"
-assert_eq "$($KC_VERSION 99999999       )" "99.99.9999" "invalid"
-
-assert_eq "$($KC_VERSION -i 0           )" "00000000" "invalid"
-assert_eq "$($KC_VERSION -i 1           )" "00000001" "invalid"
-assert_eq "$($KC_VERSION -i 00.00.1234  )" "00001234" "invalid"
-assert_eq "$($KC_VERSION -i 00.42.0000  )" "00420000" "invalid"
-assert_eq "$($KC_VERSION -i 69.00.0000  )" "69000000" "invalid"
-assert_eq "$($KC_VERSION -i 12.34.5678  )" "12345678" "invalid"
-assert_eq "$($KC_VERSION -i 99.99.9999  )" "99999999" "invalid"
+assert_eq "$($VERSION -i 0          )" "00000000" "invalid"
+assert_eq "$($VERSION -i 1          )" "00000001" "invalid"
+assert_eq "$($VERSION -i 00.00.1234 )" "00001234" "invalid"
+assert_eq "$($VERSION -i 00.42.0000 )" "00420000" "invalid"
+assert_eq "$($VERSION -i 69.00.0000 )" "69000000" "invalid"
+assert_eq "$($VERSION -i 12.34.5678 )" "12345678" "invalid"
+assert_eq "$($VERSION -i 99.99.9999 )" "99999999" "invalid"
 
 
 # Prepare current, next and previous year as variables
@@ -53,22 +45,12 @@ CY="$(date +"%y")"
 NY=$((CY + 1))
 PY=$((CY - 1))
 
-# Version bump
-assert_eq "$($KC_VERSION --major 12.34.9956 &> /dev/null)$?" "3" "invalid"
-assert_eq "$($KC_VERSION --minor 12.34.5699 &> /dev/null)$?" "3" "invalid"
+# Version bump-
+assert_eq "$($VERSION --major "00.$CY.0000" )" "00.$CY.0100" "invalid"
+assert_eq "$($VERSION --major "12.$CY.3456" )" "12.$CY.3500" "invalid"
+assert_eq "$($VERSION --major "78.$CY.9012" )" "78.$CY.9100" "invalid"
+assert_eq "$($VERSION --major "78.$CY.9012" )" "78.$CY.9100" "invalid"
 
-assert_eq "$($KC_VERSION --major "00.00.0000"   )" "00.$CY.0100" "invalid"
-assert_eq "$($KC_VERSION --major "42.$PY.1234"  )" "42.$CY.0100" "invalid"
-assert_eq "$($KC_VERSION --major "01.$NY.2468"  )" "01.$NY.2500" "invalid"
-assert_eq "$($KC_VERSION --minor "69.00.0000"   )" "69.$CY.0100" "invalid"
-assert_eq "$($KC_VERSION --minor "99.$PY.9999"  )" "99.$CY.0100" "invalid"
-assert_eq "$($KC_VERSION --minor "01.$NY.2468"  )" "01.$NY.2469" "invalid"
-
-assert_eq "$($KC_VERSION --major "00.$CY.0000"  )" "00.$CY.0100" "invalid"
-assert_eq "$($KC_VERSION --major "12.$CY.3456"  )" "12.$CY.3500" "invalid"
-assert_eq "$($KC_VERSION --major "78.$CY.9012"  )" "78.$CY.9100" "invalid"
-assert_eq "$($KC_VERSION --major "78.$CY.9012"  )" "78.$CY.9100" "invalid"
-
-assert_eq "$($KC_VERSION --minor "00.$CY.0000"  )" "00.$CY.0001" "invalid"
-assert_eq "$($KC_VERSION --minor "12.$CY.3456"  )" "12.$CY.3457" "invalid"
-assert_eq "$($KC_VERSION --minor "78.$CY.9012"  )" "78.$CY.9013" "invalid"
+assert_eq "$($VERSION --minor "00.$CY.0000" )" "00.$CY.0001" "invalid"
+assert_eq "$($VERSION --minor "12.$CY.3456" )" "12.$CY.3457" "invalid"
+assert_eq "$($VERSION --minor "78.$CY.9012" )" "78.$CY.9013" "invalid"
