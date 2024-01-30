910938401059ac8f29040e6148930a2a10895326
Niklas Bettgen <niklas@bettgen.de>
2024-01-30 16:02:45 +0100

    No leading integer zeros

--- version.sh
+++ version.sh
@@ -83,7 +83,8 @@ fi
 
 # Output parsed version
 if $INTEGER; then
-    printf "%02d%02d%02d%02d\n" "$customer" "$year" "$major" "$minor"
+    printf -v output "%02d%02d%02d%02d\n" "$customer" "$year" "$major" "$minor"
+    echo "$(trim_leading_zeros "$output")"
 else
     printf "%02d.%02d.%02d%02d\n" "$customer" "$year" "$major" "$minor"
 fi
