6e8a349941db8ea1cbb7512d5c8280965b307a05
Niklas Bettgen <niklas@bettgen.de>
2024-02-14 12:15:12 +0100

    Run tests more dynamically

--- tests/run.sh
+++ tests/run.sh
@@ -6,9 +6,7 @@ export VERSION
 
 source "$dir/assert/assert.sh"
 
-
-# Run separate unit tests
-source "$dir/test-git.sh"
-source "$dir/test-args.sh"
-source "$dir/test-parse.sh"
-source "$dir/test-bump.sh"
+# Run all unit test files
+for file in "$dir"/test*.sh; do
+   source "$file"
+done
