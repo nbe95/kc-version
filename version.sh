a7f2dd14500daa12692eda8b4ba94891f05c9869
Niklas Bettgen <niklas@bettgen.de>
2024-09-20 16:21:48 +0200

    Adapt release naming

--- version.sh
+++ version.sh
@@ -5,20 +5,20 @@ set -e
 
 # shellcheck disable=SC2001
 trim_leading_zeros() { echo "$1" | sed 's/^0\+\(.\)/\1/'; }
-usage() { echo "Usage: $(basename "$0") [-h|--help] [-v|--verbose] [-i|--integer] [--major|--minor] [VERSION]" 1>&2; exit 0; }
+usage() { echo "Usage: $(basename "$0") [-h|--help] [-v|--verbose] [-i|--integer] [--bump-minor|--bump-patch] [VERSION]" 1>&2; exit 0; }
 
 # Parse argument options
 VERBOSE=false
 INTEGER=false
-BUMP_MAJOR=false
 BUMP_MINOR=false
+BUMP_PATCH=false
 while true; do
   case "$1" in
     -h | --help ) usage ;;
     -v | --verbose ) VERBOSE=true; shift ;;
     -i | --integer ) INTEGER=true; shift ;;
-    --major ) BUMP_MAJOR=true; shift ;;
-    --minor ) BUMP_MINOR=true; shift ;;
+    --bump-minor ) BUMP_MINOR=true; shift ;;
+    --bump-patch ) BUMP_PATCH=true; shift ;;
     * ) break ;;
   esac
 done
@@ -36,8 +36,8 @@ fi
 # Try to match dot form (XX.YY.ZZZZ)
 if [[ "$version" =~ ^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,4})$ ]]; then
     customer="$(trim_leading_zeros "${BASH_REMATCH[1]}")"
-    year="$(trim_leading_zeros "${BASH_REMATCH[2]}")"
-    majmin="$(trim_leading_zeros "${BASH_REMATCH[3]}")"
+    major="$(trim_leading_zeros "${BASH_REMATCH[2]}")"
+    minor_patch="$(trim_leading_zeros "${BASH_REMATCH[3]}")"
 
 else
     # Match an integer else (pad with zeros and replace anything but numbers)
@@ -50,50 +50,50 @@ else
     printf -v version "%08d" "$(trim_leading_zeros "$version")"
 
     customer="$(trim_leading_zeros "${version:0:2}")"
-    year="$(trim_leading_zeros "${version:2:2}")"
-    majmin="$(trim_leading_zeros "${version:4:4}")"
+    major="$(trim_leading_zeros "${version:2:2}")"
+    minor_patch="$(trim_leading_zeros "${version:4:4}")"
 fi
 
 # Parse remaining components
-major=$((majmin / 100))
-minor=$((majmin % 100))
+minor=$((minor_patch / 100))
+patch=$((minor_patch % 100))
 
 # Perform version bump if requested
-if $BUMP_MAJOR || $BUMP_MINOR; then
+if $BUMP_MINOR || $BUMP_PATCH; then
     current_year="$(date +"%y")"
-    if [[ "$year" < "$current_year" ]]; then
-        # Year has changed -> reset all
-        year="$current_year"
-        major=1
-        minor=0
-    elif $BUMP_MAJOR; then
-        # Bump major, reset minor
-        major="$((major + 1))"
-        minor=0
-    else
-        # Bump minor
+    if [[ "$major" < "$current_year" ]]; then
+        # Year/major has changed -> reset all
+        major="$current_year"
+        minor=1
+        patch=0
+    elif $BUMP_MINOR; then
+        # Bump minor, reset patch
         minor="$((minor + 1))"
+        patch=0
+    else
+        # Bump patch
+        patch="$((patch + 1))"
     fi
 fi
 
-if [ "$major" -gt 99 ] || [ "$minor" -gt 99 ]; then
+if [ "$minor" -gt 99 ] || [ "$patch" -gt 99 ]; then
     echo "Cannot bump version number, because numerical limit is reached."
     exit 3
 fi
 
 # Output parsed version
 if $INTEGER; then
-    printf -v output "%02d%02d%02d%02d\n" "$customer" "$year" "$major" "$minor"
+    printf -v output "%02d%02d%02d%02d\n" "$customer" "$major" "$minor" "$patch"
     trim_leading_zeros "$output"
 else
-    printf "%02d.%02d.%02d%02d\n" "$customer" "$year" "$major" "$minor"
+    printf "%02d.%02d.%02d%02d\n" "$customer" "$major" "$minor" "$patch"
 fi
 
 # Verbose output
 if $VERBOSE; then
     echo ""
-    printf "Customer ID:\t\t%d\n" "$customer"
-    printf "Deployment year:\t20%02d\n" "$year"
-    printf "Major version:\t\t%d\n" "$major"
-    printf "Minor version:\t\t%d\n" "$minor"
+    printf "Customer:\t%d\n" "$customer"
+    printf "Major:\t\t20%02d\n" "$major"
+    printf "Minor:\t\t%d\n" "$minor"
+    printf "Patch:\t\t%d\n" "$patch"
 fi
