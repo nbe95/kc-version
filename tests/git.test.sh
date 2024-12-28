473ce1c0f6291e1905b37f34aebe9592de72b306
Niklas Bettgen <niklas@bettgen.de>
2024-12-28 15:28:46 +0100

    Update naming convention

--- tests/git.test.sh
+++ tests/git.test.sh
@@ -4,5 +4,5 @@
 # Check if version can be retrieved from latest Git tag
 git_dir="$(dirname "$0")"
 git -C "$git_dir" tag -a "99.42.69" -m "Created during unit test" &> /dev/null
-assert_eq "$($VERSION)" "99.42.0069" "Git tag"
+assert_eq "$($CMD)" "99.42.0069" "Git tag"
 git -C "$git_dir" tag -d "99.42.69" &> /dev/null
